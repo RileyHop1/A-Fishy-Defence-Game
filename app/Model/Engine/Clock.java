933b1a93538e27cf2e51b727ed86459729f17d08
Riley <rileyhop1@gmail.com>
2025-08-24 09:39:26 -0700

    Added a clock class to control the runtime of the game.

--- app/Model/Engine/Clock.java
+++ app/Model/Engine/Clock.java
@@ -1,4 +1,46 @@
 package Model.Engine;
 
+import javafx.animation.AnimationTimer;
+
 public class Clock {
+    private final long myIntervalNanos = 1_000_000_000L / 30; // target 30 ticks/sec
+    private long myLastTime = 0;
+    private AnimationTimer myTimer;
+
+    public Clock() {
+
+    }
+
+    public void start() {
+        myTimer = new AnimationTimer() {
+            @Override
+            public void handle(final long theNow) {
+
+                //First frame: just init myLastTime.
+                if (myLastTime == 0) {
+                    myLastTime = theNow;
+                    return;
+                }
+
+                long elapsed = theNow - myLastTime;
+                if (elapsed >= myIntervalNanos) {
+                    double deltaSeconds = elapsed / 1_000_000_000.0;
+
+                    Engine.processEvents();
+
+                    //reset myLastTime, and ovoid time drift.
+                    myLastTime = theNow - (elapsed - myIntervalNanos);
+                }
+
+
+            }
+        };
+        myTimer.start();
+    }
+
+    public void stop() {
+        if (myTimer != null) {
+            myTimer.stop();
+        }
+    }
 }
--- app/Model/Engine/Engine.java
+++ app/Model/Engine/Engine.java
@@ -9,19 +9,37 @@ final public class Engine {
     /**This is the queue of events that the entities want, to take.*/
     private static final Queue<Event> myEventQueue = new LinkedList<>();
 
+    private static final Engine myInstance = new Engine();
+
+    /**
+     * Private constructor to enforce singleton.
+     * */
+    private Engine() {
+        super();
+    }
+
+
+    /**
+     * This is how you get the instance of the Engine.
+     * @return A reference to the class object.
+     */
+    public static Engine getMyInstance() {
+        return myInstance;
+    }
+
     /**
      * This adds events to the queue to be processed.
      *
      * @param theEvent This is the event that will be added to the back
      *                 of the queue.
-     * */
+     */
     public static void  addEvent(final Event theEvent) {
         myEventQueue.add(theEvent);
     }
 
     /**
-     * This will return the event queue
-     * @return This is the event queue object
+     * This will return the event queue.
+     * @return This is the event queue object.
      * */
     public static Queue<Event> getMyEventQueue() {
         return myEventQueue;
@@ -29,7 +47,7 @@ final public class Engine {
 
     /**
      * This will execute all events in the event queue.
-     * */
+     */
     public static void processEvents() {
 
         for(Event e : myEventQueue) {
