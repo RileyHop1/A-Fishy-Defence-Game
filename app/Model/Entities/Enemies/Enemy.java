4dc320f1166f5771bdf7c0b3ffb58fb0c94acb3b
Riley <rileyhop1@gmail.com>
2025-08-24 17:39:35 -0700

    Filled out more of the enemy file

--- app/Model/Entities/Enemies/Enemy.java
+++ app/Model/Entities/Enemies/Enemy.java
@@ -2,10 +2,18 @@ package Model.Entities.Enemies;
 
 import Model.Entities.Type;
 
+import java.util.ArrayList;
+
 public class Enemy {
 
     private int myHealth;
     private int mySpeed;
+    /**This is the terrain type the enemy can move through.*/
+    private ArrayList<Type> myVaildPath = new ArrayList<>();
+
+    private Enemy() {
+        super();
+    }
 
     public int getMyHealth() {
         return myHealth;
@@ -21,6 +29,13 @@ public class Enemy {
 
     public void setMySpeed(int mySpeed) {
         this.mySpeed = mySpeed;
+    }
+
+    /**
+     * This moves the enemy further down the path.
+     */
+    public static void move() {
+
     }
 
 
